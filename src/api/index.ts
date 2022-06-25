101cd61e460fb1ad6a5b3a9a594b9157808ef5b6
THAISA HONORIO MORANDINI <22452@unifacef.edu.br>
2022-06-25 17:03:07 -0300

    Corrigi para trazer as variavéis de ambiente

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
+import queues from '../queues' 
 import * as express from 'express';
 const router = express.Router();
 
@@ -6,7 +7,13 @@ const getPing = async (_req, res) => {
   return res.send(body).status(200);
 };
 
+const postLog = async (req, res) => {
+  const body = { message: req.body.message };
+  await queues.log.add(body);
+  return res.send(body).status(200);
+};
  
 router.get('/', getPing);
+router.post('/log', postLog);
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { loadEnvs } from './tools/envs.tools';
-loadEnvs(`${__dirname}/../envs/.env-${process.env.NODE_ENV}`);
+loadEnvs(`${__dirname}/../envs/.env-${process.env.NODE_ENV || 'development'}`);
 import './server';
 
 process.on('uncaughtException', (error: Error) => {
